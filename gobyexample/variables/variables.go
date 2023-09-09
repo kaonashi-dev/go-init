77af80e68f4e46f55f56413273b1866e628dfe5a
Samuel <samueldlh.dev@gmail.com>
2023-09-09 00:13:28 -0500

    variables

---
+++ gobyexample/variables/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Variables
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	// Constants
+
+	const s string = "constant"
+
+	fmt.Println(s)
+	const n = 500000000
+	fmt.Println(n)
+
+}
